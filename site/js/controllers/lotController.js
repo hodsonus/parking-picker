ddc44ffc67264973a101e57c0a74ba58ae3dd257
Gabriel Guzman <gabe@collectiveand.co>
2018-11-13 16:17:20 -0500

    lots load from database, updated fullness for schema

--- site/js/controllers/lotController.js
+++ site/js/controllers/lotController.js
@@ -1,35 +1,57 @@
-angular.module('lots').controller('LotsController', ['$scope', 'Lots', 
-  function($scope, Lots) {
+const MAP_LAYER_NAME = 'parkinglots';
+
+angular.module('lots').controller('LotsController', ['$scope', 'Lots',
+  function ($scope, Lots) {
     /* Get all the lots, then bind it to the scope */
-    Lots.getAll().then(function(response) {
+    console.log('in controller', $scope);
+    Lots.getAll().then(function (response) {
       $scope.lots = response.data;
-      console.log(response);
-    }, function(error) {
+      // TODO race condition
+      if (!map.loaded()) {
+        setTimeout($scope.updateMapLayer.bind(this), 750);
+      } else $scope.updateMapLayer();
+
+      console.log('res', response);
+    }, function (error) {
       console.log('Unable to retrieve lots:', error);
     });
 
-    // $scope.detailedInfo = undefined;
+    $scope.updateMapLayer = function () {
+      var data = $scope.lots;
+      console.log(data, 'UPDATING');
+      if (map.getLayer(MAP_LAYER_NAME)) map.removeLayer(MAP_LAYER_NAME);
 
-   //  $scope.addListing = function() {
-	  // /*
-   //  Save the article using the Listings factory. If the object is successfully 
-	  // saved redirect back to the list page. Otherwise, display the error
-	  // */
+      map.addLayer({
+        'id': MAP_LAYER_NAME,
+        'type': 'fill',
+        'source': {
+          'type': 'geojson',
+          'data': data
+        },
+        'layout': {},
+        'paint': {
+          'fill-color': '#088',
+          'fill-opacity': 0.8
+        }
+      });
+      console.log('map after update layer', map);
 
-   //    Listings.create($scope.newListing);
-   //    $scope.newListing = {};
-   //    location.reload();
-   //  };
+      map.on('click', MAP_LAYER_NAME, function (e) {
+        new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(e.features[0].properties.name)
+          .addTo(map);
+      });
 
-   //  $scope.deleteListing = function(index) {
-	  //  /*
-   //   Delete the article using the Listings factory. If the removal is successful, 
-	  //  navigate back to 'listing.list'. Otherwise, display the error. 
-   //   */
+      // Change the cursor to a pointer when the mouse is over the states layer.
+      map.on('mouseenter', MAP_LAYER_NAME, function () {
+        map.getCanvas().style.cursor = 'pointer';
+      });
 
-   //    var listing = $scope.listings[index];
-   //    Listings.delete(listing._id);
-   //    location.reload();
-   //  };
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', MAP_LAYER_NAME, function () {
+        map.getCanvas().style.cursor = '';
+      });
+    }
   }
 ]);
