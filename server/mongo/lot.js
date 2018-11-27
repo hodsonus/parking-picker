7ff4cf9c380570bd175f18b2520de572c2985cc7
Gabriel Guzman <gabe@collectiveand.co>
2018-11-27 13:06:34 -0500

    cleanup, removed fullness rating enum

--- server/mongo/lot.js
+++ server/mongo/lot.js
@@ -5,7 +5,6 @@ var currentOccupencySchema = new mongoose.Schema({
   fullness: {
     type: Number,
     required: true,
-    enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   }
 }, { _id: false });
 
--- site/js/app.js
+++ site/js/app.js
@@ -1,4 +1,4 @@
-/* register the modules the application depends upon here*/
+/* register the modules the application depends upon here */
 angular.module('lots', ['Lots, LotsController']);
 /* register the application and inject all the necessary dependencies */
 var app = angular.module('parkingpicker', ['lots']);
